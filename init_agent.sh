49f26aa3217df1b72f7abad3ee983e69c0a7bbc2
grogu01 <Guenter.Grossberger@ca.com>
2016-05-10 02:44:11 -0700

    update for 10.2

--- init_agent.sh
+++ init_agent.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-# copy BRTM jars
-cp $WILY_HOME/examples/APM/BRTM/ext/*.jar $WILY_HOME/core/ext
+# copy Browser Agent jars
+cp $WILY_HOME/examples/APM/BrowserAgent/ext/*.jar $WILY_HOME/core/ext
 
-# set EM host
-sed -Ei "s/introscope.agent.enterprisemanager.transport.tcp.host.DEFAULT=localhost/introscope.agent.enterprisemanager.transport.tcp.host.DEFAULT=$EM_HOST/" $WILY_HOME/core/config/IntroscopeAgent.profile
+# set EM host and port
+sed -Ei "s/agentManager.url.1=localhost:5001/agentManager.url.1=$EM_HOST:$EM_PORT/" $WILY_HOME/core/config/IntroscopeAgent.profile
 
-# set EM port
-sed -Ei "s/introscope.agent.enterprisemanager.transport.tcp.port.DEFAULT=5001/introscope.agent.enterprisemanager.transport.tcp.port.DEFAULT=$EM_PORT/" $WILY_HOME/core/config/IntroscopeAgent.profile
-
-# enable BRTM
-sed -Ei 's/#introscope.agent.brtm.enabled=false/introscope.agent.brtm.enabled=true/' $WILY_HOME/core/config/IntroscopeAgent.profile
-sed -Ei 's/#brtm.pbd/brtm.pbd/' $WILY_HOME/core/config/tomcat-typical.pbl
+# enable Browser Agent
+sed -Ei 's/#introscope.agent.browseragent.enabled=false/introscope.agent.browseragent.enabled=true/' $WILY_HOME/core/config/IntroscopeAgent.profile
+sed -Ei 's/#browseragent.pbd/browseragent.pbd/' $WILY_HOME/core/config/tomcat-typical.pbl
 
 # now add the APM agent to Tomcat startup parameters
 export CATALINA_OPTS="$CATALINA_OPTS -Xmx2048m -javaagent:$WILY_HOME/Agent.jar -Dcom.wily.introscope.agentProfile=$WILY_HOME/core/config/IntroscopeAgent.profile -Dcom.wily.introscope.agent.agentName=$AGENT_NAME"
